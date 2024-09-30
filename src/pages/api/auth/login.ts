a60e922dd52de7bd5c3d3574b69f1fd1c4525508
akiria5552 <timeo59150@outlook.fr>
2024-09-30 17:09:12 +0200

    Update login.ts

--- src/pages/api/auth/login.ts
+++ src/pages/api/auth/login.ts
@@ -11,7 +11,7 @@ export default function handler(req: NextApiRequest, res: NextApiResponse) {
     'https://discord.com/api/oauth2/authorize?' +
     new URLSearchParams({
       client_id: CLIENT_ID,
-      redirect_uri: `${getAbsoluteUrl()}/api/auth/callback`,
+      redirect_uri: `https://discord.com/oauth2/authorize?client_id=1290328015821082654&permissions=8&integration_type=0&scope=bot`,
       response_type: 'code',
       scope: 'identify guilds',
       state: locale ?? '',
